bd543c49c934358d577dfd1c8eceda1f9d1a41cb
Mashrafi Al Maruf <masrafi3366@gmail.com>
2026-09-01 23:24:01 +0600

    Completed Phase 3 - Added Entity Framework setup and database connection

---
+++ StudentRentalSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace StudentRentalSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options
+            )
+            : base(options)
+        {
+
+        }
+
+    }
+}
--- StudentRentalSystem/Program.cs
+++ StudentRentalSystem/Program.cs
@@ -1,29 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using StudentRentalSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+// Add MVC Services
 builder.Services.AddControllersWithViews();
+
+
+// Add Database Connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// Configure HTTP Request Pipeline
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+
 app.UseRouting();
+
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+
+// Default Route
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 
 app.Run();
