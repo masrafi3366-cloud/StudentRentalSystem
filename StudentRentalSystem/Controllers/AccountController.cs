3f97fcdf21fc368e875e8a71af45c4ad90718fc4
Mashrafi Al Maruf <masrafi3366@gmail.com>
2026-09-02 19:00:51 +0600

    Completed Phase 9 - Added student dashboard and profile system

--- StudentRentalSystem/Controllers/AccountController.cs
+++ StudentRentalSystem/Controllers/AccountController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using StudentRentalSystem.Data;
+using StudentRentalSystem.Models;
 
 
 namespace StudentRentalSystem.Controllers
@@ -12,6 +12,7 @@ namespace StudentRentalSystem.Controllers
         private readonly ApplicationDbContext _context;
 
 
+
         public AccountController(ApplicationDbContext context)
         {
             _context = context;
@@ -19,112 +20,71 @@ namespace StudentRentalSystem.Controllers
 
 
 
-        // Login Page
-
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-
-
-        // Login Submit
-
-
-        [HttpPost]
-        public IActionResult Login(string email, string password)
-        {
-
-
-            var student = _context.Students
-                .FirstOrDefault(
-                x => x.Email == email
-                &&
-                x.Password == password
-                );
-
-
-
-            if (student == null)
-            {
-
-                ViewBag.Error =
-                "Invalid email or password";
-
-                return View();
-
-            }
-
-
-
-            if (student.IsApproved == false)
-            {
-
-                ViewBag.Error =
-                "Your account is waiting for admin approval";
-
-
-                return View();
-
-            }
-
-
-
-            HttpContext.Session.SetInt32(
-                "StudentId",
-                student.StudentId
-            );
-
-
-            HttpContext.Session.SetString(
-                "StudentName",
-                student.FullName
-            );
-
-
-
-            return RedirectToAction(
-                "Dashboard"
-            );
-
-        }
-
-
-
-
-
         public IActionResult Dashboard()
         {
 
+            int? studentId =
+            HttpContext.Session.GetInt32("StudentId");
 
-            var name =
-            HttpContext.Session.GetString(
-                "StudentName"
+
+            if (studentId == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+
+
+            var student =
+            _context.Students
+            .FirstOrDefault(
+                x => x.StudentId == studentId
             );
 
 
-            ViewBag.Name = name;
-
-
-            return View();
+            return View(student);
 
         }
 
 
+
+
+
+        public IActionResult Profile()
+        {
+
+
+            int? studentId =
+            HttpContext.Session.GetInt32("StudentId");
+
+
+            if (studentId == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+
+
+            var student =
+            _context.Students
+            .FirstOrDefault(
+                x => x.StudentId == studentId
+            );
+
+
+
+            return View(student);
+
+        }
 
 
 
         public IActionResult Logout()
         {
 
-
             HttpContext.Session.Clear();
 
 
-            return RedirectToAction(
-                "Login"
-            );
-
+            return RedirectToAction("Login");
 
         }
 
