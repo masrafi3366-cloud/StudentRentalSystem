84ab57d52ebd3aa27f1416ebc9aaae95e3bdccb9
Mashrafi Al Maruf <masrafi3366@gmail.com>
2026-09-03 11:36:30 +0600

    Completed Phase 25.1 - Added decimal precision configuration for financial fields

--- StudentRentalSystem/Data/ApplicationDbContext.cs
+++ StudentRentalSystem/Data/ApplicationDbContext.cs
@@ -19,22 +19,106 @@ namespace StudentRentalSystem.Data
 
 
 
+
+
+
+
         public DbSet<Student> Students { get; set; }
+
 
 
         public DbSet<Item> Items { get; set; }
 
 
+
         public DbSet<Rental> Rentals { get; set; }
+
 
 
         public DbSet<Payment> Payments { get; set; }
 
 
+
         public DbSet<ExtraCharge> ExtraCharges { get; set; }
 
 
+
         public DbSet<Admin> Admins { get; set; }
+
+
+
+
+
+
+
+
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder
+        )
+        {
+
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+
+
+
+            // Item Price Precision
+
+
+            modelBuilder.Entity<Item>()
+                .Property(x => x.PricePerDay)
+                .HasPrecision(18, 2);
+
+
+
+
+
+
+
+            // Rental Total Amount Precision
+
+
+            modelBuilder.Entity<Rental>()
+                .Property(x => x.TotalAmount)
+                .HasPrecision(18, 2);
+
+
+
+
+
+
+
+            // Payment Amount Precision
+
+
+            modelBuilder.Entity<Payment>()
+                .Property(x => x.Amount)
+                .HasPrecision(18, 2);
+
+
+
+
+
+
+
+            // Extra Charge Amount Precision
+
+
+            modelBuilder.Entity<ExtraCharge>()
+                .Property(x => x.Amount)
+                .HasPrecision(18, 2);
+
+
+
+        }
+
+
+
 
 
     }
